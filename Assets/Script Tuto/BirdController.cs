a16685859160e92f104adfe58825cf1ac0bde136
ShizuK8 <jurgen20112024@gmail.com>
2026-04-14 11:43:09 +0200

    Pipes Move
    Les pipes qui bougent + rigidbody des pipes

--- Assets/Script Tuto/BirdController.cs
+++ Assets/Script Tuto/BirdController.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class BirdController : MonoBehaviour
-
-    
 {
-    private Rigidbody2D _rigidbody2D;
 
-    public float jumpForce;
+    private Rigidbody2D rb;
 
-    private void Start()
+    [Header("Bird Settings")]
+    [Range(1f, 10f)]
+    public float jumpForce = 5f;
+
+
+    void Jump()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        if (_rigidbody2D == null) Debug.LogError("Bird Controll, pas de rigidbody");
+        rb.linearVelocity = Vector2.zero;
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-        _rigidbody2D.AddForceY(jumpForce, ForceMode2D.Impulse);
-        }
-        
+        HandleInput();
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -10f, 10f));
     }
 }
+
